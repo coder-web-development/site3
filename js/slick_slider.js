012687b0169236a3551d5bf588c8680bfa12330b
coder-web-development <80450304+coder-web-development@users.noreply.github.com>
2021-03-25 16:07:27 +0500

    Add files via upload

---
+++ js/slick_slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows:true,
+		dots:false,
+		adaptiveHeight:false,
+		slidesToShow:3,
+		slidesToScroll:3,
+		speed:300,
+		easing:'linear',
+		infinite:true,
+		initialSlide:0,
+		//autoplay:false,
+		//autoplaySpeed:3000,
+		//pauseOnFocus:true,
+		//pauseOnHover:true,
+		//pauseOnDotsHover:true,
+		//draggable:false,
+		//swipe:false,
+		//touchThreshold:5,
+		//touchMove:true,
+		//waitForAnimate:true,
+		//centerMode:true,
+		variableWidth:true,
+		//rows:1,
+		//slidesPerRow:1,
+		//vertical:false,
+		//verticalSwiping:false,
+	});	
+}); 
+
+
+
+
+
+
+
